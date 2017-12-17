f0d305146c221dc7abbb6fb8efdfe110dd5fe0be
Brandon Leeds <brandonleeds@gmail.com>
2017-12-17 22:10:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    find_by_name(name) unless find_by_name(name) == nil 
+    find_by_name(name) unless find_by_name(name) == nil
+    create_by_name(name) 
   end
 end
