8ef1c4b870b8aa382f86b7c26a40e65e3fd9f8ef
Brandon Leeds <brandonleeds@gmail.com>
2017-12-17 22:07:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
     @@all << song
     song
   end
-
+  
   def self.all
     @@all
   end
@@ -29,6 +29,6 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.filter { |song| song.name == name }
+    @@all.select { |song| song.name == name }
   end
 end
