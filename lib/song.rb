6cb8cb55731290926d28be907cb8551852da8f9f
Brandon Leeds <brandonleeds@gmail.com>
2017-12-17 22:29:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     song
   end
 
-  def self.create_fron_filename(filename)
+  def self.create_from_filename(filename)
     song = new_from_filename(filename)
     @@all << song
     song
